8d29e1041fe580fa21fc295e75ac3906e9e41274
B3ka-code <b9914246@gmail.com>
2026-04-23 16:41:17 +0500

    Update and rename import aiohttp.py to Python работа с сетями.py

--- Python работа с сетями.py
+++ Python работа с сетями.py
@@ -1,23 +1,3 @@
-# import aiohttp
-# import asyncio
-# import json
-# import os
-
-# async def posts():
-#     url = 'https://jsonplaceholder.typicode.com/posts'
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             posts = await response.json()
-#             os.makedirs('data', exist_ok=True)
-#         for item in posts:
-#             file_path = f"data_folder/file_{item['id']}.json"
-
-#             with open('hi.json', 'w', encoding="utf_8") as f:
-#                 json.dump(posts, f, ensure_ascii=False, indent=3 )
-#                 print("всеэ сохранено")
-
-# if __name__ == "__main__":
-#     asyncio.run(posts())
 
 import aiohttp
 import asyncio
